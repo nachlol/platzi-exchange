6fdf418dc8431d4f525782d675abda224ca171b2
Nicolas Chavez <Nicolas Chavez>
2020-09-22 12:50:58 -0400

    Project Platzi :D

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import { VueSpinners } from '@saeris/vue-spinners'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+import router from '@/router'
+import { dollarFilter, percentFilter } from '@/filters'
+
+// Componentes de terceros
+
+Vue.use(VueSpinners)
+Vue.use(Chartkick.use(Chart))
+
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
